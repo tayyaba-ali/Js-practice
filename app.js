fd7686c9f1719fc55dc260fc654f10b3eb27710a
Tayyaba Ali <98578632+tayyaba-ali@users.noreply.github.com>
2023-04-10 03:08:34 +0500

    sum of 10 natural numbers

--- app.js
+++ app.js
@@ -1,49 +1,43 @@
-// console.log("Javascript practice");
-// console.log("Javascript by harshit");
+// grade="Javascript practice");
+// grade="Javascript by harshit");
 
 // trim
 
-
 // let myName = "   Tayyaba  ";
 
-// console.log(myName.length);
+// grade=myName.length);
 
 // let myTRim = myName.trim()
-// console.log(myTRim.length);
-
+// grade=myTRim.length);
 
 // myName= myName.toUpperCase()
-// console.log(myName.toUpperCase());
-// console.log(myName.toLowerCase());
-// console.log(myName);
-
+// grade=myName.toUpperCase());
+// grade=myName.toLowerCase());
+// grade=myName);
 
 // let userName = prompt("Enter your name");
-// console.log(userName);
+// grade=userName);
 // let trueValue = userName.slice(0, 1).toUpperCase() + userName.slice(1).toLowerCase();
 
 // userName = userName.slice(0, 1).toUpperCase() + userName.slice(1).toLowerCase();
 
-// console.log(userName);
+// grade=userName);
 
 // let userArr = userName.split(" ")
-// console.log(userArr);
+// grade=userArr);
 // for (let word of userArr) {
 //     //    userArr=word.slice(0,1).toLowerCase()+word.slice(1).toLowerCase()
 //     let updatedArr=[]
 //     word = word.slice(0, 1).toLowerCase() + word.slice(1).toLowerCase();
 //     updatedArr.push(word)
-//     console.log(word);
+//     grade=word);
 // }
 
 // for (let i = 0; i < userArr; i++){
-//     console.log(i.slice(0, 1).toUpperCase() + i.slice(1).toLowerCase());
+//     grade=i.slice(0, 1).toUpperCase() + i.slice(1).toLowerCase());
 // }
 
-
-// console.log(userArr);
-
-
+// grade=userArr);
 
 // //Title Case
 
@@ -55,10 +49,9 @@
 //     word= word.slice(0,1).toUpperCase()+word.slice(1).toLowerCase()
 //     updatedSentence.push(word);
 // }
-// console.log(updatedSentence);
+// grade=updatedSentence);
 // updatedSentence= updatedSentence.join(" ")
 // document.write(`The title case of your sentence is <br>   <b>${updatedSentence}</b> `);
-
 
 //title Case
 
@@ -67,3 +60,82 @@
 // let titleCaseArr = userArr.map((word) => word.slice(0, 1).toUpperCase() + word.slice(1).toLowerCase());
 // titleCaseArr = titleCaseArr.join(" ");
 // document.write(`The titleCase of your sentence is <br> <b><i>${titleCaseArr}</i></b>`)
+
+// let userAge = 90;
+
+// function allowOrNot (userAge) {
+//     if (userAge > 18) {
+//         grade="You are allowrd")
+//         return;
+//     }
+//     grade="You are not allowed");
+// }
+
+// allowOrNot(userAge)
+
+// let userPercentage = +prompt("Enter your percentage");
+// if (userPercentage <= 100 && userPercentage>=90) {
+//     grade="A+");
+// }
+// else if(userPercentage<90 && userPercentage>=80){
+//     grade="A")
+// }
+
+// else if (userPercentage < 80 && userPercentage >= 70) {
+//     grade="B");
+// }
+
+// else if (userPercentage < 70 && userPercentage >= 60) {
+//     grade="c");
+// }
+// else if (userPercentage <= 50  && userPercentage>0) {
+//     grade="F");
+// }
+// else {
+//     grade="Invalid percentage")
+// }
+
+// with switch statement
+
+// let userPercent = +prompt('Enter your percentage');
+// let grade;
+
+// switch (true) {
+// 	case userPercent <= 100 && userPercent >= 90:
+// 		grade='A+';
+//         break;
+    
+//     case (userPercent < 90 && userPercent >= 80):
+//         grade="A";
+//         break;
+//     case (userPercent < 80 && userPercent >= 70):
+//         grade="B";
+//         break;
+//     case (userPercent < 70 && userPercent >= 60):
+//         grade="C"
+//         break;
+//     case (userPercent < 60 && userPercent >= 50):
+//         grade = "D";
+//         break;
+    
+//     case (userPercent < 50 && userPercent > 0):
+//         grade="F";
+//         break;
+//     default:
+//         grade="Invalid percentage";
+//         break
+// }
+// document.write(grade);
+
+// sum of 10 natural numbers by while loop
+
+let i = 1;
+let sum = 0;
+while (i <= 10) {
+    sum = sum + i;
+    i++;
+}
+
+console.log(sum)
+
+
