7d9d70469008f57e7838d2d74630cb3b99434f08
tayyaba-ali <ta6402792@gmail.com>
2023-05-23 17:38:48 +0500

    template literals

--- app.js
+++ app.js
@@ -1,6 +1,4 @@
-
-"use strict"
-
+'use strict';
 
 // Starting Javascript
 // comments
@@ -8,7 +6,6 @@
 // console.log("hello world");
 // console.log('Tayyaba');
 // console.log(`hi my name is tayyaba`);
-
 
 // var myName = "Tayyaba";
 // console.log(myName);
@@ -22,13 +19,11 @@
 
 // console.log(window.student);
 
-
 // rules for naming variables
 // camelcase is standard
 
 // var $myName = "Tayyaba";
 // console.log($myName);
-
 
 // let
 
@@ -38,23 +33,54 @@
 
 // console.log(myName);
 
-
 // constants
 
 // const pi = 3.142;
 
-
-
 // console.log(pi);
 
-
 // string indexing
- 
-let myName = "tayyaba";
 
-console.log(myName[3]);
-console.log(myName[6]);
-// console.log(myName[7]);
+// let myName = "tayyaba";
 
-//last index 
-console.log(myName[myName.length-1]);
+// console.log(myName[3]);
+// console.log(myName[6]);
+// // console.log(myName[7]);
+
+// //last index
+// console.log(myName[myName.length-1]);
+// console.log(myName[myName.length-2]);
+// console.log(myName[myName.length-3]);
+// console.log(myName[myName.length-4]);
+
+// string methods
+
+// let myName = "  Tayyaba   ";
+// console.log(myName.length);
+
+// let trimmedName=name.trim()
+// console.log(trimmedName.length);
+
+// name = name.trim().length
+// console.log(name);
+
+// myName.toUpperCase()
+
+// string concatenation
+
+// let firstName = '15';
+// let lastName = 15;
+
+// let fullName = firstName-lastName;
+// console.log( typeof fullName);
+
+// console.log("hello");
+
+// template literals
+
+let fsName = "Tayyaba";
+let lsName = "Ali";
+
+let fullName = `My name is ${fsName} and my father name is ${lsName}`
+
+console.log(fullName);
